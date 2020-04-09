efa680dd8cd36d0eabb33d676cc625db0f80afeb
unknown <tangbaphuc2906@gmail.com>
2020-04-09 14:34:44 +0700

    'ok'

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -27,4 +27,42 @@ $(document).ready(function() {
             $(".buttontop").removeClass("show");
         }
     });
+    // new Glider(document.querySelector('.glider'), {
+    //     slidesToScroll: 1,
+    //     slidesToShow: 5.5,
+    //     draggable: true,
+    //     dots: '.dots',
+    //     arrows: {
+    //         prev: '.glider-prev',
+    //         next: '.glider-next'
+    //     }
+    // });
+    // document.querySelector('.glider').addEventListener('glider-slide-visible', function(event) {
+    //     var imgs_to_anticipate = 3;
+    //     var glider = Glider(this);
+    //     for (var i = 0; i <= imgs_to_anticipate; ++i) {
+    //         var index = Math.min(event.detail.slide + i, glider.slides.length - 1),
+    //             glider = glider;
+    //         loadImages.call(glider.slides[index], function() {
+    //             glider.refresh(true);
+    //         })
+    //     }
+    // });
+
+    // function loadImages(callback) {
+    //     [].forEach.call(this.querySelectorAll('img'), function(img) {
+    //         var _img = new Image,
+    //             _src = img.getAttribute('data-src');
+    //         _img.onload = function() {
+    //             img.src = _src;
+    //             img.classList.add('loaded');
+    //             callback && callback(img);
+    //         }
+    //         if (img.src !== _src) _img.src = _src;
+    //     });
+    // }
+    $('#blogCarousel').carousel({
+        interval: 5000;
+        autoplay = true
+    });
 });
